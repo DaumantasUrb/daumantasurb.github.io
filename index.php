d424130ded83691cd94fabc9010f40587de8516e
Daumantas Urbanavičius <daumantasurbanavicius@Daumantass-MacBook-Pro.local>
2023-12-04 10:42:47 +0200

    second

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<html>
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+<body>
+<link rel="stylesheet" type="text/css" href="mgallery.css"/>
+
+
+<script src="pinchzoom2.js?v=3"></script>
+<script src="mgallery.js?v=3"></script>
+<script>
+    initMGallery();
+</script>
+</body>
+</html>
